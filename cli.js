c8386f59f05a3a167a1ae8d0ebe6057b820c38e8
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-10-03 12:00:01 -0500

    Adding make commands

--- cli.js
+++
@@ -1,8 +0,0 @@
-// src/cli.js
-import readlineSync from 'readline-sync';
-
-export const greetUser = () => {
-    console.log('¡Bienvenido a Brain Games');
-    const name = readlineSync.question('¿Cual es tu nombre? ');
-    console.log('¡Hola, ${name}!');
-};
