9d075ff85a82f05d4f0216caf29f8227a1bc1373
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-10-23 10:55:41 -0500

    Complete

--- src/calc.js
+++ src/calc.js
@@ -2,25 +2,13 @@ import readlineSync from 'readline-sync';
 import greetUser from './cli.js';
 
 const operations = ['+', '-', '*'];
-const randomOperation = () => operations[Math.floor(Math.random() * operations.length)];
-const calculate = (n1, n2, operator) => {
-    switch (operator) {
-        case '+':
-            return n1 + n2;
-        case '-':
-            return n1 - n2;
-        case '*':
-            return n1 * n2;
-        default: 
-            throw new Error(`Operación Desconocida: $(operation)`);
-    }
-};
+
 const play = () => {
 const userName = greetUser();
 console.log('¿Cuál es el resultado de la expresión?');
 
     const round = 3;
-    for (let i=0; i < round; i++) {
+    for (let i = 0; i < round; i++) {
        
     }
     console.log(`¡Felicidades, ${userName}!`);
