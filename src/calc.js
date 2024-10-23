bdeb6167961cc866b817c7db7e91422b624764b9
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-10-23 10:51:09 -0500

    Complete

--- src/calc.js
+++ src/calc.js
@@ -4,41 +4,47 @@ import greetUser from './cli.js';
 const operations = ['+', '-', '*'];
 const randomOperation = () => operations[Math.floor(Math.random() * operations.length)];
 const calculate = (n1, n2, operator) => {
-    switch (operator) {
-        case '+':
-            return n1 + n2;
-        case '-':
-            return n1 - n2;
-        case '*':
-            return n1 * n2;
-        default: 
-            throw new Error(`Operación Desconocida: $(operation)`);
-    }
+  switch (operator) {
+    case '+':
+      return n1 + n2;
+    case '-':
+      return n1 - n2;
+    case '*':
+      return n1 * n2;
+    default:
+      throw new Error('Operación Desconocida: $(operation)');
+  }
 };
+
 const play = () => {
-const userName = greetUser();
-console.log('¿Cuál es el resultado de la expresión?');
+  console.log('¡Bienvenido a Brain Games!');
 
-    const round = 3;
-    for (let i=0; i < round; i++) {
-        const n1 = Math.floor(Math.random() * 100);
-        const n2 = Math.floor(Math.random() * 100);
-        const operator = randomOperation();
+  const userName = greetUser();
+  console.log('¿Cuál es el resultado de la expresión?');
 
-        const correctAnswer = calculate(n1, n2, operator).toString();
-        
-        console.log(`Pregunta: ${n1} ${operator} ${n2}`);
-        const userAnswer = readlineSync.question('Tu respuesta: ');
+  const round = 3;
+  for (let i = 0; i < round; i += 1) {
+    const n1 = Math.floor(Math.random() * 100);
+    const n2 = Math.floor(Math.random() * 100);
+    const operator = randomOperation();
 
-        if (userAnswer === correctAnswer) {
-            console.log('¡Correcto!');
-        } else {
-            console.log(`'${userAnswer}' es una respuesta incorrecta ;(. La respuesta correcta era '${correctAnswer}'.`);
-            console.log(`¡Intentémoslo de nuevo, ${userName}!`);
-            return;
-        }
+    const correctAnswer = calculate(n1, n2, operator).toString();
+
+    console.log(`Pregunta: ${n1} ${operator} ${n2}`);
+    const useAnswer = readlineSync.question('Tu respuesta: ');
+
+    if (useAnswer === correctAnswer) {
+      console.log('¡Correcto!');
+    } else {
+      console.log(
+        `'${useAnswer}' es una respuesta incorrecta. La respuesta correcta era '${correctAnswer}'.`,
+      );
+      console.log(`¡Intentémoslo de nuevo, ${userName}!`);
+      return;
     }
-    console.log(`¡Felicidades, ${userName}!`);
+  }
+
+  console.log(`¡Felicidades, ${userName}!`);
 };
 
-export default play;
+export default play();
