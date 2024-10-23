f2e7b3d923a07b1d83a98aa4ba83c6ea7fdce603
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-10-23 09:46:53 -0500

    Complete

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greetUser = () => {
   console.log('¡Bienvenido a Brain Games!');
-  const userName = readlineSync.question(' ¿Cual es tu nombre? ');
+  const userName = readlineSync.question('¿Cual es tu nombre?');
   return userName;
 };
 
