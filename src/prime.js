57fc843583f38ee810dbc16f888d2b4111af14fe
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-11-18 11:30:09 -0500

    Adding make commands

--- src/prime.js
+++ src/prime.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 import greetUser from './cli.js';
 
 const prime = (num) => {
-  if (num <= 1) return false;
+  if (num <= 2) return false;
   for (let i = 2; i <= Math.sqrt(num); i+=1) {
     if (num % i === 0) return false;
   }
