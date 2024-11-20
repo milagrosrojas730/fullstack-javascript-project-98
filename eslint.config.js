bb9e985bee1e27893defbb83081955684732f830
Clara Rojas Levano <milagrosrojas730@gmail.com>
2024-11-20 10:22:26 -0500

    Adding make commands

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import globals from 'globals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const compat = new FlatCompat({
+  baseDirectory: dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: { import: importPlugin },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+    },
+  },
+  ...compat.extends('airbnb-base'),
+  {
+    rules: {
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/no-amd': 'off',
+      'import/no-mutable-exports': 'off',
+      'import/newline-after-import': 'off',
+      'import/first': 'off',
+    },
+  },
+];
--- src/prime.js
+++ src/prime.js
@@ -2,15 +2,15 @@ import readlineSync from 'readline-sync';
 import greetUser from './cli.js';
 
 const prime = (num) => {
-    if (num <= 2) {
+  if (num <= 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
       return false;
     }
-    for (let i = 2; i <= Math.sqrt(num); i += 1) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 };
 
 const play = () => {
@@ -20,17 +20,17 @@ const play = () => {
 
   const round = 3;
 
-    for (let i = 0; i < round; i += 1) {
+  for (let i = 0; i < round; i += 1) {
     const num = Math.floor(Math.random() * 100);
     const correctAnswer = prime(num) ? 'yes' : 'no';
     const userAnswer = readlineSync.question(`Pregunta: ${num}  .Tu respuesta: `).toLowerCase();
-if (userAnswer !== 'yes' && userAnswer !== 'no') {
-            console.log(`"${userAnswer}" no es una respuesta válida. Intenta nuevamente.`);
-            return;
-        }
-  if (userAnswer === correctAnswer) {
+    if (userAnswer !== 'yes' && userAnswer !== 'no') {
+      console.log(`"${userAnswer}" no es una respuesta válida. Intenta nuevamente.`);
+      return;
+    }
+    if (userAnswer === correctAnswer) {
       console.log('¡Correcto!');
-  } else {
+    } else {
       console.log(`'${userAnswer}' es incorrecto. La respuesta correcta era '${correctAnswer}'`);
       console.log(`¡Intentémoslo de nuevo, ${userName}!`);
       return;
